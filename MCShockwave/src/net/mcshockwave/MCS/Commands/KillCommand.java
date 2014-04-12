e2db29a4c9d4858a83f368d05200d1dbc45c0de9
ShadowLAX <ShadowLAX@users.noreply.github.com>
2014-04-12 13:06:30 -0700

    Fixed Format so it is somewhat legible.

--- MCShockwave/src/net/mcshockwave/MCS/Commands/KillCommand.java
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/KillCommand.java
@@ -9,16 +9,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class KillCommand implements CommandExecutor {
+	
 @Override
 public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	if (sender instanceof Player && SQLTable.hasRank(sender.getName(), Rank.JR_MOD))
-    if (cmd.getName().equalsIgnoreCase("KillPlayer")) {
-        Player target = sender.getServer().getPlayer(args[0]);
-        if (target == null) {
-            sender.sendMessage(args[0] + " is not online.");
-            return true;
-        }
-        target.setHealth(0); 
-    }{
-    return false;
-}}}
+	if (sender instanceof Player && SQLTable.hasRank(sender.getName(), Rank.JR_MOD)) {
+		Player target = sender.getServer().getPlayer(args[0);
+                if (target == null) {
+                    sender.sendMessage(args[0] + " is not online.");
+                    return true;
+         	}
+         	Player target = sender.getServer().getPlayer(args[0]);
+                target.setHealth(0);
+	}
+        return false;
+}
