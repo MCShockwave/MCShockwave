46c75bb24af3961ec1d4edcb668cdfe2fa14276e
CarroCake <CrystalPrism@icloud.com>
2014-04-12 15:42:10 -0400

    Added kill and edited upsies

---
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/KillCommand.java
@@ -0,0 +1,24 @@
+package net.mcshockwave.MCS.Commands;
+
+import net.mcshockwave.MCS.SQLTable;
+import net.mcshockwave.MCS.SQLTable.Rank;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class KillCommand implements CommandExecutor {
+@Override
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	if (sender instanceof Player && SQLTable.hasRank(sender.getName(), Rank.JR_MOD))
+    if (cmd.getName().equalsIgnoreCase("KillPlayer")) {
+        Player target = sender.getServer().getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(args[0] + " is not online.");
+            return true;
+        }
+        target.setHealth(0); 
+    }{
+    return false;
+}}}
