f3464d4d0c777ce8d57b396acdf170a5aa516b00
CarroCake <CrystalPrism@icloud.com>
2014-05-01 16:44:07 -0400

    why not

---
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/CriCommand.java
@@ -0,0 +1,23 @@
+package net.mcshockwave.MCS.Commands;
+
+import net.mcshockwave.MCS.SQLTable;
+import net.mcshockwave.MCS.SQLTable.Rank;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CriCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		Player player = (Player) sender;
+		if (SQLTable.hasRank(sender.getName(), Rank.ENDER)) {
+			player.chat(ChatColor.AQUA + ";-; i cri evertim");
+		}
+		return false;
+	}
+}
