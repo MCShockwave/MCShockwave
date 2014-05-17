7bec8c7aa2bbf148836c9a4538488eec46089184
Blaxcraft <golfboy2300@yahoo.com>
2014-05-17 09:09:16 -0700

    Fixed CustomEntityRegistrar.java

--- MCShockwave/src/net/mcshockwave/MCS/Entities/CustomEntityRegistrar.java
+++ MCShockwave/src/net/mcshockwave/MCS/Entities/CustomEntityRegistrar.java
@@ -1,6 +1,10 @@
 package net.mcshockwave.MCS.Entities;
 
+import net.minecraft.server.v1_7_R2.BiomeBase;
+import net.minecraft.server.v1_7_R2.BiomeMeta;
 import net.minecraft.server.v1_7_R2.Entity;
+import net.minecraft.server.v1_7_R2.EntityInsentient;
+import net.minecraft.server.v1_7_R2.EntityTypes;
 import net.minecraft.server.v1_7_R2.World;
 
 import org.bukkit.Location;
@@ -8,60 +12,67 @@ import org.bukkit.craftbukkit.v1_7_R2.CraftWorld;
 import org.bukkit.entity.EntityType;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class CustomEntityRegistrar {
 
-	protected static Field	mapStringToClassField, mapClassToStringField, mapClassToIdField, mapStringToIdField;
+	public static void addCustomEntity(String name, EntityType shown, Class<? extends EntityInsentient> nmsClass,
+			Class<? extends EntityInsentient> customClass) {
+		@SuppressWarnings("deprecation")
+		int id = shown.getTypeId();
 
-	static {
 		try {
-			mapStringToClassField = net.minecraft.server.v1_7_R2.EntityTypes.class.getDeclaredField("c");
-			mapClassToStringField = net.minecraft.server.v1_7_R2.EntityTypes.class.getDeclaredField("d");
-			mapClassToIdField = net.minecraft.server.v1_7_R2.EntityTypes.class.getDeclaredField("f");
-			mapStringToIdField = net.minecraft.server.v1_7_R2.EntityTypes.class.getDeclaredField("g");
 
-			mapStringToClassField.setAccessible(true);
-			mapClassToStringField.setAccessible(true);
-			mapClassToIdField.setAccessible(true);
-			mapStringToIdField.setAccessible(true);
+			List<Map<?, ?>> dataMaps = new ArrayList<Map<?, ?>>();
+			for (Field f : EntityTypes.class.getDeclaredFields()) {
+				if (f.getType().getSimpleName().equals(Map.class.getSimpleName())) {
+					f.setAccessible(true);
+					dataMaps.add((Map<?, ?>) f.get(null));
+				}
+			}
+
+			if (dataMaps.get(2).containsKey(id)) {
+				dataMaps.get(0).remove(name);
+				dataMaps.get(2).remove(id);
+			}
+
+			Method method = EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, int.class);
+			method.setAccessible(true);
+			method.invoke(null, customClass, name, id);
+
+			for (Field f : BiomeBase.class.getDeclaredFields()) {
+				if (f.getType().getSimpleName().equals(BiomeBase.class.getSimpleName())) {
+					if (f.get(null) != null) {
+
+						for (Field list : BiomeBase.class.getDeclaredFields()) {
+							if (list.getType().getSimpleName().equals(List.class.getSimpleName())) {
+								list.setAccessible(true);
+								@SuppressWarnings("unchecked")
+								List<BiomeMeta> metaList = (List<BiomeMeta>) list.get(f.get(null));
+
+								for (BiomeMeta meta : metaList) {
+									Field clazz = BiomeMeta.class.getDeclaredFields()[0];
+									if (clazz.get(meta).equals(nmsClass)) {
+										clazz.set(meta, customClass);
+									}
+								}
+							}
+						}
+
+					}
+				}
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked", "deprecation" })
-	public static void addCustomEntity(Class aiClass, String name, EntityType typeShown) {
-		// protected static Field mapIdToClassField;
-
-		short id = typeShown.getTypeId();
-
-		if (mapStringToClassField == null || mapStringToIdField == null || mapClassToStringField == null
-				|| mapClassToIdField == null) {
-			return;
-		} else {
-			try {
-				Map mapStringToClass = (Map) mapStringToClassField.get(null);
-				Map mapStringToId = (Map) mapStringToIdField.get(null);
-				Map mapClasstoString = (Map) mapClassToStringField.get(null);
-				Map mapClassToId = (Map) mapClassToIdField.get(null);
-
-				mapStringToClass.put(name, aiClass);
-				mapStringToId.put(name, Integer.valueOf(id));
-				mapClasstoString.put(aiClass, name);
-				mapClassToId.put(aiClass, Integer.valueOf(id));
-
-				mapStringToClassField.set(null, mapStringToClass);
-				mapStringToIdField.set(null, mapStringToId);
-				mapClassToStringField.set(null, mapClasstoString);
-				mapClassToIdField.set(null, mapClassToId);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static <E> Entity spawnCustomEntity(Class<? extends net.minecraft.server.v1_7_R2.Entity> entClass, Location loc) {
+	public static <E> Entity spawnCustomEntity(Class<? extends net.minecraft.server.v1_7_R2.Entity> entClass,
+			Location loc) {
 		try {
 			World w = ((CraftWorld) loc.getWorld()).getHandle();
 			Entity ent = entClass.getConstructor(World.class).newInstance(w);
