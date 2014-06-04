c7f9c6cdda39de2537e21d3172b84f45656bdd9b
CarroCake <CrystalPrism@icloud.com>
2014-06-03 20:17:57 -0400

    /loaf - &c<3 much loaf

---
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/LoafCommand.java
@@ -0,0 +1,44 @@
+package net.mcshockwave.MCS.Commands;
+
+import net.mcshockwave.MCS.MCShockwave;
+import net.mcshockwave.MCS.SQLTable;
+import net.mcshockwave.MCS.SQLTable.Rank;
+import net.mcshockwave.MCS.Utils.CooldownUtils;
+import net.mcshockwave.MCS.Utils.PacketUtils;
+import net.mcshockwave.MCS.Utils.PacketUtils.ParticleEffect;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class LoafCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (SQLTable.hasRank(sender.getName(), Rank.OBSIDIAN)) {
+				if (CooldownUtils.isOnCooldown("/loaf", player.getName())) {
+					MCShockwave.send(
+							player,
+							"You must wait %s seconds to use /loaf again!",
+							CooldownUtils.getCooldownForSec("/loaf",
+									player.getName(), 1));
+					return false;
+				}
+				player.chat(ChatColor.RED + "<3 much loaf");
+
+				CooldownUtils.addCooldown("/" + label, player.getName(), 2400);
+			} else {
+				MCShockwave
+						.send(player,
+								"You must be %s to use /loaf! Buy VIP at buy.mcshockwave.net",
+								"Obsidian+");
+			}
+		}
+		return false;
+	}
+}
