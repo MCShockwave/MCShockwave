d5d90d3e951d74187c7c508c1fa19eda93274822
Blaxcraft <golfboy2300@yahoo.com>
2014-06-08 18:38:21 -0700

    Added setop file to git

---
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/SetOpCommand.java
@@ -0,0 +1,89 @@
+package net.mcshockwave.MCS.Commands;
+
+import net.mcshockwave.MCS.MCShockwave;
+import net.mcshockwave.MCS.SQLTable;
+import net.mcshockwave.MCS.SQLTable.Rank;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.sql.SQLException;
+
+public class SetOpCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (SQLTable.hasRank(sender.getName(), Rank.ADMIN) || sender == Bukkit.getConsoleSender()) {
+			if (args.length == 0) {
+				sender.sendMessage("§c/setop {name} {servers}");
+				sender.sendMessage("§cServers: +servertoadd -servertoremove +@ for all");
+			} else if (args.length > 2) {
+				String player = args[1];
+				for (int i = 2; i < args.length; i++) {
+					String server = args[i];
+					if (server.startsWith("+")) {
+						String ser = server.replaceFirst("+", "");
+						if (ser.equalsIgnoreCase("@")) {
+							for (String in : MCShockwave.servers) {
+								try {
+									if (!MCShockwave.isOp(player, server)) {
+										addOpFor(player, in);
+										sender.sendMessage("§aOpped " + player + " on " + in);
+									}
+								} catch (SQLException e) {
+								}
+							}
+						} else {
+							addOpFor(player, ser);
+							sender.sendMessage("§aOpped " + player + " on " + ser);
+						}
+					} else if (server.startsWith("-")) {
+						String ser = server.replaceFirst("-", "");
+						if (ser.equalsIgnoreCase("@")) {
+							for (String in : MCShockwave.servers) {
+								try {
+									if (MCShockwave.isOp(player, server)) {
+										removeOpFor(player, in);
+										sender.sendMessage("§cDe-opped " + player + " on " + in);
+									}
+								} catch (SQLException e) {
+								}
+							}
+						} else {
+							removeOpFor(player, ser);
+							sender.sendMessage("§cDe-opped " + player + " on " + ser);
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public void addOpFor(String player, String server) {
+		if (!SQLTable.OPS.has("Username", player)) {
+			SQLTable.OPS.add("Username", player, "Servers", ";" + server);
+		} else {
+			String cur = SQLTable.OPS.get("Username", player, "Servers");
+			cur += ";" + server;
+			SQLTable.OPS.set("Servers", cur, "Username", player);
+		}
+	}
+
+	public void removeOpFor(String player, String server) {
+		if (SQLTable.OPS.has("Username", player)) {
+			String cur = SQLTable.OPS.get("Username", player, "Servers");
+			cur.replaceAll(";" + server, "");
+			if (cur.length() > 0) {
+				SQLTable.OPS.set("Servers", cur, "Username", player);
+			} else {
+				SQLTable.OPS.del("Username", player);
+			}
+		}
+	}
+
+}
