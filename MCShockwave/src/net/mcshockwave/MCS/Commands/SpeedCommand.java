fefc521500e350a074f1280b94b629968597b1c9
ShadowLAX <cody09061999@gmail.com>
2014-07-09 18:47:39 -0700

    Access to set other's flyspeed

--- MCShockwave/src/net/mcshockwave/MCS/Commands/SpeedCommand.java
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/SpeedCommand.java
@@ -1,5 +1,6 @@
 package net.mcshockwave.MCS.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,16 +13,25 @@ public class SpeedCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
-			if (args.length == 1 && sender.isOp()) {
+			if (args.length >= 1 && sender.isOp()) {
+				float speed = 0;
 				try {
-					float speed = Float.parseFloat(args[0]);
-					p.setFlySpeed((float) speed / 10.0f);
-					p.sendMessage("Your speed is now " + speed);
+					speed = Float.parseFloat(args[0]);
 				} catch (IllegalArgumentException e) {
 					p.sendMessage(ChatColor.RED + "Invalid speed: " + args[0] + ", must be from 1 to 10.");
 				}
-			} else
-				p.setFlySpeed((float) 0.1);
+				if (args.length == 1) {
+					p.setFlySpeed((float) speed / 10.0f);
+					p.sendMessage("Your speed is now " + speed);
+					return true;
+				}
+				if (args.length == 2) {
+					Player t = Bukkit.getPlayer(args[1]);
+					t.setFlySpeed((float) speed / 10.0f);
+					p.sendMessage(t.getName() + "'s speed is now " + speed);
+					return true;
+				}
+			}
 		}
 
 		return false;
