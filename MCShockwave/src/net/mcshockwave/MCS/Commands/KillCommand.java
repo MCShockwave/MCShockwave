60eb1e63f66d87f42b1e2366775cd093ade5c479
CarroCake <CrystalPrism@icloud.com>
2014-08-13 11:25:47 -0400

    i was told to do a thing

--- MCShockwave/src/net/mcshockwave/MCS/Commands/KillCommand.java
+++ MCShockwave/src/net/mcshockwave/MCS/Commands/KillCommand.java
@@ -15,15 +15,23 @@ public class KillCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (SQLTable.hasRank(sender.getName(), Rank.JR_MOD)) {
 			if (args.length < 1) {
-				sender.sendMessage("§cSyntax error: /killplayer {name}");
+				sender.sendMessage("§cSyntax error: /killplayer {name} [optional reason]");
 				return false;
 			}
+			String reason = "";
+			for(String s : args) {
+			        if(s != args[0]) {
+			                reason += s + " ";
+			        }
+			}
+
 			Player target = Bukkit.getPlayer(args[0]);
 			if (target == null) {
 				sender.sendMessage("§c" + args[0] + " is not online.");
 				return true;
 			}
 			target.setHealth(0f);
+			target.sendMessage("§7Killed by§6 " + sender.getName() + " §7for §6" + reason);
 		}
 		return false;
 	}
